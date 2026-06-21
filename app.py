efca1b44788012a221d13b00eef02d98059cd5c7
aabhi04 <84304661+aabhi04@users.noreply.github.com>
2026-06-21 23:33:55 +0530

    Improve missing value handling and add model training
    Refactor data handling for missing values and model training.

--- app.py
+++ app.py
@@ -12,17 +12,38 @@ if uploaded_file is not None:
     st.write("Column Data Types:")
     st.write("Datatypes:",df.dtypes)
     target = st.selectbox("Select a column to analyze", df.columns, key="column_selector")
-    y = df[target]
-    X = df.drop(target, axis=1)
     st.write(f"Analyzing column: {target}")
-    st.write(f"Features: {X.shape[1]} columns, Target: {target}")
     st.write("Missing Values:")
     st.write(df.isnull().sum())
     missing_value_strategy = st.selectbox("How to handle missing values?", ["Drop rows", "Fill with mean"], key="missing_value_strategy")
     if missing_value_strategy == "Drop rows":
-        X = X.dropna()
+       df_clean = df.dropna()
     else:
-        X = X.fillna({col: X[col].mean() if X[col].dtype in ['float64', 'int64'] else X[col].mode()[0] for col in X.columns})
+        df_clean = df.fillna({col: df[col].mean() if df[col].dtype in ['float64', 'int64'] else df[col].mode()[0] for col in df.columns})
+    
+    X = df_clean.drop(target, axis=1)
+    y = df_clean[target]
+    st.write(f"Features: {X.shape[1]} columns, Target: {target}")
     X = pd.get_dummies(X)
     test_size = st.slider("Test Size", min_value=0.1, max_value=0.5, value=0.2, step=0.05, key="test_size_slider")
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    st.selectbox("Select a model to train", ["Logistic Regression","Decision tree","KNN", "Random Forest"], key="model_selector")
+    if st.button("Train Model", key="train_button"):
+        model_name = st.session_state.model_selector
+        
+        if model_name == "Logistic Regression":
+            from sklearn.linear_model import LogisticRegression
+            model = LogisticRegression()
+        elif model_name == "Decision Tree":
+            from sklearn.tree import DecisionTreeClassifier
+            model = DecisionTreeClassifier()
+        elif model_name == "KNN":
+            from sklearn.neighbors import KNeighborsClassifier
+            model = KNeighborsClassifier()
+        elif model_name == "Random Forest":
+            from sklearn.ensemble import RandomForestClassifier
+            model = RandomForestClassifier()
+        
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
